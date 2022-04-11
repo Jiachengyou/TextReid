5051116819c9bf658226fd28836b6c024d476788
JiaChengyou <1284975112@qq.com>
2022-04-11 10:29:51 +0800

    update

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ CONFIG=configs/cuhkpedes/baseline_gru_cliprn50_ls_bs128_vit.yaml
 CONFIG=configs/cuhkpedes/VIT/baseline_gru_clipvit_ls_bs128_stride14.yaml
 CONFIG=configs/cuhkpedes/VIT/baseline_textvit_clipvit_ls_bs64_stride16_fine.yaml
 # CONFIG=configs/cuhkpedes/VIT/baseline_textvit_clipvit_ls_bs64_stride16.yaml
-OUTDIR=./VIT/share_layer12_bs64_fine
-RESUME=./output/VIT/share_layer12_bs64_fine/best.pth
+OUTDIR=./VIT/share_layer12_bs64_fine_mask_v0
+RESUME=./output/VIT/share_layer12_bs64_fine_mask_v0/best.pth
 CUDA_VISIBLE_DEVICES=1 python test_net.py --config-file $CONFIG --checkpoint-file $RESUME
 # RESUNME
 # --use-tensorboard
--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ CONFIG=configs/cuhkpedes/baseline_gru_cliprn50_ls_bs128_vit.yaml
 CONFIG=configs/cuhkpedes/VIT/baseline_gru_clipvit_ls_bs128_stride14.yaml
 CONFIG=configs/cuhkpedes/VIT/baseline_textvit_clipvit_ls_bs64_stride16_fine.yaml
 # CONFIG=configs/cuhkpedes/VIT/baseline_textvit_clipvit_ls_bs64_stride16.yaml
-OUTDIR=./VIT/share_layer12_bs64_fine_mask_v0
+OUTDIR=./VIT/share_layer12_bs64_fine_mask_v1
 # RESUME=./output/VIT/baseline_textvit_clipvit_ls_bs96_stride16_test_4/best.pth
 CUDA_VISIBLE_DEVICES=0 python train_net.py --config-file $CONFIG --output-dir $OUTDIR
 # RESUNME
